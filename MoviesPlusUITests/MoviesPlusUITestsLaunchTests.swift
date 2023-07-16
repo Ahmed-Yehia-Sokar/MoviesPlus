1f986585d45ba5ad694d1fd4daa3e5b689d3f9b8
Ahmed Sokar <ahmed.sokar@fawry.com>
2023-07-16 11:00:31 +0300

    Initial Commit

---
+++ MoviesPlusUITests/MoviesPlusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesPlusUITestsLaunchTests.swift
+//  MoviesPlusUITests
+//
+//  Created by admin on 16/07/2023.
+//
+
+import XCTest
+
+final class MoviesPlusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
